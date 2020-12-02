0b424d6eb2fc958083a811953d3767ca7de18539
SigmaGF <jgfurlan2@gmail.com>
2020-12-02 00:15:53 -0300

    make it better 2

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,11 +1,12 @@
-import 'dotenv/config';
-
 import { app } from './app';
 import { authMiddleware } from './middlewares/auth.middleware';
 import { clientsRouter } from './services/clients';
 import { receiptsRouter } from './services/receipts';
 import { usersRouter } from './services/users';
 import { vehiclesRouter } from './services/vehicles';
+
+// eslint-disable-next-line global-require
+if(process.env.NODE_ENV === 'development') { require('dotenv/config'); }
 
 app.use('/users', usersRouter);
 app.use('/clients', authMiddleware, clientsRouter);
