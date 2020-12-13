e711147bed4a530ffd8fdb5641576a2fa65b5e7b
SigmaGF <jgfurlan2@gmail.com>
2020-12-13 17:28:14 -0300

    Show error 5

--- packages/server/src/services/crvs/list/controller.ts
+++ packages/server/src/services/crvs/list/controller.ts
@@ -8,21 +8,21 @@ export class CRVsListController {
   async handle(req: Request, res: Response) {
     const page = parseInt(req.query.page as string || '1', 10);
     const limit = parseInt(req.query.limit as string || '10', 10);
-    const noPagination = req.query.noPagination as string || undefined;
+    const noPagination = req.query.noPagination as string || '';
 
-    const name = req.query.name as string || undefined;
-    const document = req.query.document as string || undefined;
-    const group = req.query.group as string || undefined;
+    const name = req.query.name as string || '';
+    const document = req.query.document as string || '';
+    const group = req.query.group as string || '';
 
-    const plate = req.query.plate as string || undefined;
-    const renavam = req.query.renavam as string || undefined;
-    const brandModel = req.query.brandModel as string || undefined;
-    const type = req.query.type as string || undefined;
+    const plate = req.query.plate as string || '';
+    const renavam = req.query.renavam as string || '';
+    const brandModel = req.query.brandModel as string || '';
+    const type = req.query.type as string || '';
 
-    const details = req.query.details as string || undefined;
-    const issuedOn = req.query.issuedOn as string || undefined;
+    const details = req.query.details as string || '';
+    const issuedOn = req.query.issuedOn as string || '';
     const status = req.query.status as string || '0';
-    const licensingMonth = req.query.licensingMonth as string || undefined;
+    const licensingMonth = req.query.licensingMonth as string || '';
 
     try {
       const response = await this.service.execute({
