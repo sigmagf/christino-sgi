ae71c47f05fcf3dfe3b39afbc09c6afa837d1079
SigmaGF <jgfurlan2@gmail.com>
2020-12-13 19:17:08 -0300

    blablabla

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -16,4 +16,6 @@ app.use('/crvs', authMiddleware, crvsRouter);
 app.listen(process.env.PORT || 3000, () => {
   console.clear();
   console.log(`Christino SGI server start on port ${process.env.PORT || 3000}...`);
+  console.log(__dirname);
+  console.log(process.env.HOME);
 });
