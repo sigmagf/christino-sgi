3ea24952007abb6dcc849d33c0d642ecfc1b15c4
Sigma <jgfurlan2@gmail.com>
2020-12-14 13:25:23 -0300

    Groups to select
    CHANGE input to select and fix groups names

--- packages/web/src/pages/CRVs/Filters/index.tsx
+++ packages/web/src/pages/CRVs/Filters/index.tsx
@@ -19,7 +19,38 @@ export const CRVsFilters: React.FC<ICRVsFilter> = ({ onFilterSubmit }) => {
     onFilterSubmit(data);
   };
 
-  const options = [
+  const groupOptions = [
+    { value: '', label: 'TODOS' },
+    { value: '-1', label: 'SEM GRUPO' },
+    { value: 'ABA', label: 'ABA' },
+    { value: 'AOYAMA', label: 'AOYAMA' },
+    { value: 'AUTO PECAS', label: 'AUTO PECAS' },
+    { value: 'CGM', label: 'CGM' },
+    { value: 'CLAUDIO', label: 'CLAUDIO' },
+    { value: 'E J RIBEIRO', label: 'E J RIBEIRO' },
+    { value: 'EDMUNDO NETO', label: 'EDMUNDO NETO' },
+    { value: 'EDMUR', label: 'EDMUR' },
+    { value: 'ELIANO ANTUNES', label: 'ELIANO ANTUNES' },
+    { value: 'ELIAS ANTUNES', label: 'ELIAS ANTUNES' },
+    { value: 'ELVIS AIELO', label: 'ELVIS AIELO' },
+    { value: 'ESCRITORIO', label: 'ESCRITORIO' },
+    { value: 'F. DOMINGUES', label: 'F. DOMINGUES' },
+    { value: 'GIANSANTE LEITAO', label: 'GIANSANTE LEITAO' },
+    { value: 'GRANJA ANTUNES', label: 'GRANJA ANTUNES' },
+    { value: 'JACOB', label: 'JACOB' },
+    { value: 'KAITO', label: 'KAITO' },
+    { value: 'M DE CAMPOS', label: 'M DE CAMPOS' },
+    { value: 'PAULO GARCIA', label: 'PAULO GARCIA' },
+    { value: 'PERNINHA', label: 'PERNINHA' },
+    { value: 'PIO', label: 'PIO' },
+    { value: 'PREFEITURA', label: 'PREFEITURA' },
+    { value: 'RM DOCES', label: 'RM DOCES' },
+    { value: 'VALTER RODRIGUES', label: 'VALTER RODRIGUES' },
+    { value: 'VANIL RODRIGUES', label: 'VANIL RODRIGUES' },
+    { value: 'ZE DITO', label: 'ZE DITO' },
+  ];
+
+  const licensingMonthOptions = [
     { value: '', label: 'TODOS' },
     { value: '4', label: '4 - ABRIL' },
     { value: '5', label: '5 - MAIO' },
@@ -37,13 +68,13 @@ export const CRVsFilters: React.FC<ICRVsFilter> = ({ onFilterSubmit }) => {
       <FiltersContainer>
         <Scope path="client">
           <Input style={{ gridArea: 'CN' }} name="name" label="NOME" />
-          <Input style={{ gridArea: 'CG' }} name="group" label="GRUPO" />
+          <Select style={{ gridArea: 'CG' }} options={groupOptions} defaultValue={{value='', label='TODOS'}} name="group" label="GRUPO" />
         </Scope>
         <Scope path="vehicle">
           <Input style={{ gridArea: 'VP' }} name="plate" label="PLACA" />
           <Input style={{ gridArea: 'VR' }} name="renavam" label="RENAVAM" />
           <Input style={{ gridArea: 'VB' }} name="brandModel" label="MARCA/MODELO" />
-          <Select style={{ gridArea: 'VV' }} options={options} defaultValue={{ value: '', label: 'TODOS' }} name="licensingMonth" label="VENCIMENTO" />
+          <Select style={{ gridArea: 'VV' }} options={licensingMonthOptions} defaultValue={{ value: '', label: 'TODOS' }} name="licensingMonth" label="VENCIMENTO" />
         </Scope>
 
         <Button style={{ gridArea: 'BT' }} type="submit" apparence="success">
