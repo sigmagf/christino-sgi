593a81b56c64188523bc7054021ae27562e3ec31
Sigma <jgfurlan2@gmail.com>
2020-12-14 13:52:54 -0300

    Update styles.ts

--- packages/web/src/components/Layout/styles.ts
+++ packages/web/src/components/Layout/styles.ts
@@ -13,6 +13,6 @@ export const AppMain = styled.main`
 export const AppContent = styled.section`
   height: 100%;
   width: 100%;
-  max-width: 1140px;
+  max-width: 1080px;
   margin: 15px auto 0 auto;
 `;
