91d7312c1f157476e9bd7f00221fb7ca78164fa9
sigmagf <jgfurlan2@gmail.com>
2021-01-17 00:14:29 -0300

    Restart front-end

--- packages/web/src/pages/Vehicles/index.tsx
+++ packages/web/src/pages/Vehicles/index.tsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FaSearch as SearchIcon } from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 import { Button } from '~/components/Button';
 import { Card } from '~/components/Card';
 import { Layout } from '~/components/Layout';
 import { Pagination } from '~/components/Pagination';
 import { Table } from '~/components/Table';
+import { IPagination, IVehicle } from '~/interfaces';
+import { api } from '~/services/api';
 
 export const VehiclesPage: React.FC = () => {
   document.title = 'Veiculos | Christino';
 
+  const [vehicles, setVehicles] = useState<IPagination<IVehicle>>({
+    page: {
+      total: 1,
+      current: 1,
+      limit: 10,
+    },
+    data: [],
+  });
+  const [qsFilter, setqsFilter] = useState('?page=1&limit=10');
+
+  const getData = useCallback(async () => {
+    try {
+      const response = await api.get<IPagination<IVehicle>>(`/vehicles${qsFilter}`);
+
+      setVehicles(response.data);
+    } catch(err) {
+      if(err.message === 'Network Error') {
+        toast.error('Verifique sua conexão com a internet.');
+      } else {
+        toast.error(err.response.data.message);
+      }
+    }
+  }, []);
+
+  useEffect(() => { getData(); }, []);
+  useEffect(() => { getData(); }, [qsFilter]);
+
   return (
     <>
       <Layout>
-        <Card style={{ margin: '15px 0' }}>
-          *
-        </Card>
-
         <Card style={{ position: 'relative' }}>
           <Table>
             <thead>
@@ -29,36 +55,31 @@ export const VehiclesPage: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td style={{ fontFamily: 'monospace' }}>ALMEIDA & ANTUNES ORGANIZACAO E PROMOCAO DE FEIRAS LTDA</td>
-                <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>ABC-1234</td>
-                <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>12.345.678.998</td>
-                <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>I/VW AMAROK CD 4X4 TREND</td>
-                <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>
-                  <Button>
-                    <SearchIcon />
-                  </Button>
-                </td>
-              </tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
-              <tr><td colSpan={5} /></tr>
+              {vehicles?.data.length > 0 && (
+                vehicles?.data.map((vehicle) => (
+                  <tr>
+                    <td style={{ fontFamily: 'monospace' }}>{ vehicle.client_id }</td>
+                    <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>{ vehicle.plate }</td>
+                    <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>{ vehicle.renavam }</td>
+                    <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>{ vehicle.brand_model }</td>
+                    <td style={{ fontFamily: 'monospace', textAlign: 'center' }}>
+                      <Button>
+                        <SearchIcon />
+                      </Button>
+                    </td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </Table>
         </Card>
 
         <Card style={{ margin: '15px 0' }}>
           <Pagination
-            currentPage={1}
-            totalPages={10}
+            currentPage={vehicles.page.current}
+            totalPages={vehicles.page.total}
             inLoading={false}
-            onNumberClick={() => console.log('onPageClick')}
+            onNumberClick={(page) => setqsFilter(`?page=${page}&limit=10`)}
             onMaxResultsChange={() => console.log('onMaxResultsChange')}
           />
         </Card>
