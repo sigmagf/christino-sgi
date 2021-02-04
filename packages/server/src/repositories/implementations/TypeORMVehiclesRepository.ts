727319bd760123e3c2247d62e241a981d74bb83a
Sigma <jgfurlan2@gmail.com>
2021-02-04 13:16:20 -0300

    Update TypeORMVehiclesRepository.ts

--- packages/server/src/repositories/implementations/TypeORMVehiclesRepository.ts
+++ packages/server/src/repositories/implementations/TypeORMVehiclesRepository.ts
@@ -146,7 +146,7 @@ export class TypeORMVehiclesRepository implements IVehiclesRepository {
       brand_model: data.brand_model || oldData.brand_model,
       type: data.type || oldData.type,
       details: data.details || oldData.details,
-      status: data.status || oldData.status,
+      status: (data.status && data.status !== undefined && data.status !== null && data.status >= 0) ? data.status : oldData.status,
       crlve_included: data.crlve_included || oldData.crlve_included,
     });
 
