9dc962e6ae3a9e3b18b253ceaf29ec64e8781e4f
sigmagf <jgfurlan2@gmail.com>
2021-02-04 22:16:25 -0300

    Fix things

--- packages/server/src/services/vehicles/update/controller.ts
+++ packages/server/src/services/vehicles/update/controller.ts
@@ -21,9 +21,41 @@ export class VehiclesUpdateController {
     const brand_model = stringFix(req.body.brand_model, undefined, 'UPPERCASE');
     const type = stringFix(req.body.type, undefined, 'UPPERCASE');
     const details = stringFix(req.body.details, undefined, 'UPPERCASE');
-    const status = req.body.status || '';
+    const status = stringFix(req.body.status, undefined, 'UPPERCASE');
 
     try {
+      if(!name) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Client name is null or undefined.' }));
+      }
+
+      if(!document) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Client document is null or undefined.' }));
+      }
+
+      if(!plate) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Vehicle plate is null or undefined.' }));
+      }
+
+      if(!renavam) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Vehicle renavam is null or undefined.' }));
+      }
+
+      if(!crv) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Vehicle crv is null or undefined.' }));
+      }
+
+      if(!brand_model) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Vehicle brand_model is null or undefined.' }));
+      }
+
+      if(!type) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Vehicle type is null or undefined.' }));
+      }
+
+      if(!status) {
+        throw new Error(JSON.stringify({ code: 400, message: 'Vehicle status is null or undefined.' }));
+      }
+
       const user = await this.service.execute({ id, name, document, group, plate, renavam, crv, brand_model, type, details, status });
       return res.status(200).json(user);
     } catch(err) {
