123cda9893fee96f7e7b284df81e157faf6c4e68
João Gabriel <jgfurlan2@gmail.com>
2021-02-19 09:53:49 -0300

    Update index.tsx

--- packages/web/src/components/WorksDetailsModal/index.tsx
+++ packages/web/src/components/WorksDetailsModal/index.tsx
@@ -87,8 +87,9 @@ export const WorksDetailsModal: React.FC<IWorksDetailsModalProps> = ({ isOpen, o
         serviceId: yup.string().required('O serviço é obrigatório.'),
         value: yup.string().required('O valor é obrigatório.'),
         status: yup.string().required('O status é obrigatório.'),
-        identifier: yup.string().required('O identificador é obrigatório.'),
-        history: yup.string().required('O historico é obrigatório.'),
+        details: yup.string().max(256, 'Os detalhes devem ter no máximo 256 caracteres'),
+        identifier: yup.string().max(16, 'O identifier devem ter no máximo 16 caracteres').required('O identificador é obrigatório.'),
+        history: yup.string().max(128, 'O histórico devem ter no máximo 115 caracteres').required('O historico é obrigatório.'),
       });
 
       const history = `${worksStatus.filter((el) => el.value === data.status)[0].label} - ${data.history}`;
@@ -190,8 +191,8 @@ export const WorksDetailsModal: React.FC<IWorksDetailsModalProps> = ({ isOpen, o
           <Input disabled={!!work || !editing} name="identifier" label="IDENTIFICADOR" />
           <Input disabled={!editing} name="value" label="VALOR" onFocus={() => onValueFocus(formRef)} onBlur={() => onValueBlur(formRef)} />
           <Select isDisabled={!editing} name="status" label="STATUS" options={worksStatus} />
-          <TextArea disabled={!editing} name="details" label="DETALHES" rows={3} />
-          { editing && <Input name="history" label="NOVA ENTRADA" /> }
+          <TextArea disabled={!editing} name="details" label="DETALHES" rows={3} maxlength={256} />
+          { editing && <Input name="history" label="NOVA ENTRADA" maxlength={115} /> }
 
           <Table>
             <thead>
