35a9e23a51239a2d352d5487f325902b7777f97b
sigmagf <jgfurlan2@gmail.com>
2021-02-25 10:08:44 -0300

    Add print works

--- packages/web/src/pages/Works/index.tsx
+++ packages/web/src/pages/Works/index.tsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { FaPrint } from 'react-icons/fa';
 import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import { Layout } from '~/components/Layout';
 import { WorksDataTable } from '~/components/WorksDataTable';
 import { WorksDetailsModal } from '~/components/WorksDetailsModal';
 import { WorksFiltersCard } from '~/components/WorksFiltersCard';
 import { useSWR } from '~/hooks/useSWR';
+import { Button } from '~/interface/Button';
 import { Card } from '~/interface/Card';
 import { Paginator } from '~/interface/Paginator';
 import { IPagination, IWork } from '~/interfaces';
+import { api } from '~/utils/api';
 import { handleHTTPRequestError } from '~/utils/handleHTTPRequestError';
 import { qsConverter } from '~/utils/qsConverter';
+
+import { worksPrintScreen } from './printScreen';
 
 export const WorksPage: React.FC = () => {
   document.title = 'Ordem de Serviço | Christino';
@@ -28,6 +34,7 @@ export const WorksPage: React.FC = () => {
 
   /* - BOOLEAN STATES - */
   const [detailsModal, setDetailsModal] = useState(false);
+  const [inLoadingPrint, setInLoadingPrint] = useState(false);
   /* END BOOLEAN STATES */
 
   /* - HANDLE DETAILS MODAL - */
@@ -35,6 +42,31 @@ export const WorksPage: React.FC = () => {
   const onCreateClick = () => { setDetailsModal(true); setWorkIdToDetails(undefined); };
   const onDetailsClick = (id: string) => { setDetailsModal(true); setWorkIdToDetails(id); };
   /* END HANDLE DETAILS MODAL */
+
+  /* - HANDLE PRINT CLICK - */
+  const onPrintClick = async () => {
+    setInLoadingPrint(true);
+
+    try {
+      const response = await api.get<IWork[]>(`/works?noPagination=true&${qsConverter(filters)}`, { headers: { authorization: `Bearer ${storage.getItem('token')}` } });
+
+      // eslint-disable-next-line
+      const win = window.open('', 'TITULO', `toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,width=${screen.width},height=${screen.height}`);
+
+      if(win) {
+        win.document.body.innerHTML = worksPrintScreen(response.data);
+        win.print();
+        win.close();
+      } else {
+        toast.error('Ative o popup em seu navegador.');
+      }
+    } catch(err) {
+      handleHTTPRequestError(err);
+    }
+
+    setInLoadingPrint(false);
+  };
+  /* END HANDLE PRINT CLICK */
 
   useEffect(() => {
     if(getWorkError) {
@@ -45,6 +77,12 @@ export const WorksPage: React.FC = () => {
   if(workPermission === 0) {
     return <Navigate to="/" replace />;
   }
+
+  const printButton = (
+    <Button variant="info" disabled={inLoadingPrint} style={{ cursor: inLoadingPrint ? 'progress' : 'pointer' }} onClick={onPrintClick}>
+      <FaPrint />&nbsp;&nbsp;&nbsp;IMPRIMIR
+    </Button>
+  );
 
   return (
     <>
@@ -67,6 +105,7 @@ export const WorksPage: React.FC = () => {
             currentPage={works?.page.current || 1}
             inLoading={inLoading}
             onNumberClick={(page) => setFilters((old) => ({ ...old, page }))}
+            leftContent={printButton}
           />
         </Card>
       </Layout>
---
+++ packages/web/src/pages/Works/printScreen.ts
@@ -0,0 +1,48 @@
+import { IWork } from '~/interfaces';
+import { worksStatus } from '~/utils/commonSelectOptions';
+import { formatDate, formatMoney } from '~/utils/formatString';
+
+export function worksPrintScreen(vehicles: IWork[]) {
+  return `
+    <head>
+      <title>IMPRESSAO DOS VEÍCULOS</title>
+    </head>
+    <body>
+      <style>
+        @page { size: landscape; -webkit-print-color-adjust: exact; }
+        * { font-family: 'Roboto Mono', monospace; font-size: 10px; }
+        html, body { margin: 0; padding: 0; }
+        table { width: 100%; border-radius: 5px; overflow: hidden; border-collapse: collapse; }
+        td, th { padding: 8px; }
+        thead > tr > th{ font-weight: 800; }
+        tbody > tr:nth-child(even) { background: white; }
+        tbody > tr:nth-child(odd) { background: lightgray; }
+      </style>
+
+      <table>
+        <thead>
+          <tr>
+            <th style="text-align: left">CLIENTE</th>
+            <th>IDENTIFICADOR</th>
+            <th>SETOR/SERVIÇO</th>
+            <th>VALOR</th>
+            <th>STATUS</th>
+            <th>CRIADO EM</th>
+          </tr>
+        </thead>
+        <tbody>
+        ${vehicles.map((w) => `
+          <tr>
+            <td>${w.client.document.padStart(14, '*')} - ${w.client.name}</td>
+            <td style="text-align: center">${w.identifier}</td>
+            <td style="text-align: center">${w.service.sector.name}/${w.service.name}</td>
+            <td style="text-align: center">${formatMoney(w.value)}</td>
+            <td style="text-align: center">${worksStatus.find((e) => e.value === w.status.toString())?.label}</td>
+            <td style="text-align: center">${formatDate(w.createdAt)}</td>
+          </tr>
+        `).join('')}
+        </tbody>
+      </table>
+    </body>
+  `;
+}
